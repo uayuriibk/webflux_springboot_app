78e8426a7c256fabf5688a97b9f9c229a12f190d
yuriybk <email2yurii@gmail.com>
2020-02-27 23:40:09 +0200

    springboot starter skeleton has been added

---
+++ src/main/java/com/yuriib/springboot/webflux_springboot_app/WebfluxApp.java
@@ -0,0 +1,13 @@
+package com.yuriib.springboot.webflux_springboot_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxApp.class, args);
+	}
+
+}
---
+++ src/test/java/com/yuriib/springboot/webflux_springboot_app/WebfluxAppTests.java
@@ -0,0 +1,13 @@
+package com.yuriib.springboot.webflux_springboot_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
