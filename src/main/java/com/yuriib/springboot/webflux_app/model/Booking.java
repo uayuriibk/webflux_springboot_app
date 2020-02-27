90ff2c94b5aaa31e1fd3b703288dc0c58a78d4e2
yuriybk <email2yurii@gmail.com>
2020-02-27 23:54:15 +0200

    mongodb related dependencies have been added. minor refactoring

---
+++ src/main/java/com/yuriib/springboot/webflux_app/model/Booking.java
@@ -0,0 +1,28 @@
+package com.yuriib.springboot.webflux_app.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Document
+public class Booking {
+
+    @Id
+    private String id;
+
+    private Long roomNumber;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    private LocalDate checkIn;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    private LocalDate checkOut;
+
+    private Integer price;
+}
